f1449d1a1a8ab43abad2f62ab6e2d1390d1171a0
Shawn Miller <hereiamnow@gmail.com>
2025-08-09 17:19:13 +0000

    Create a component from SectionThemeSwap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 import { useState } from 'react'
 import Header from '@/components/layout/header'
@@ -12,6 +13,7 @@ import RoxyIcon from '@/components/roxy-icon'
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import ThemeSwap from '@/components/theme-swap'
 
 function HeroSection() {
   return (
@@ -274,41 +276,9 @@ function ThemeSection({ onThemeChange }: { onThemeChange: (theme: 'dark' | 'ligh
               The Dark Collection Perfect for low-light environments like your favorite cigar lounge or
               for users who prefer a bolder, modern aesthetic.
             </p>
-
-            <label className="flex cursor-pointer gap-2 justify-center items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round">
-                <circle cx="12" cy="12" r="5" />
-                <path
-                  d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-              </svg>
-              <input 
-                type="checkbox" 
-                className="toggle theme-controller" 
-                onChange={(e) => onThemeChange(e.target.checked ? 'dark' : 'light')}
-                defaultChecked={true}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round">
-                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-              </svg>
-            </label>
+            <div className="flex justify-center">
+              <ThemeSwap onThemeChange={onThemeChange} defaultChecked={true} />
+            </div>
           </div>
         </div>
       </div>
